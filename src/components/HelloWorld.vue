99fdd7cfaab85d24ce7533c36ac9364076f5c74f
Grace <gxu660@aucklanduni.ac.nz>
2025-02-13 21:57:19 +1300

    Set up people input

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,84 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const people = ref([]);
+const personName = ref("");
+const selectedPerson = ref("");
+
+const addPerson = () => {
+  //push whatever's in the input field to the people array
+  if (personName.value.trim()) {
+    people.value.push(personName.value);
+    personName.value = "";
+  }
+  console.log(people.value);
+};
+
+const formatNames = () => {
+  if (people.value.length === 0) {
+    return "Welcome...";
+  }
+  let welcomemsg = "Welcome,";
+  for (let i = 0; i < people.value.length; i++) {
+    if (people.value.length >= 2 && i == people.value.length - 1) {
+      welcomemsg += ` and ${people.value[i]}`;
+    } else if (i !== 0) {
+      welcomemsg += `, ${people.value[i]}`;
+    } else {
+      welcomemsg += ` ${people.value[i]}`;
+    }
+  }
+  return welcomemsg;
+};
+
+const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div id="app">
+    <div>
+      <div>
+        <h1>Expense Tracker</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+        <!-- add participants -->
+        <div>
+          <input
+            v-model="personName"
+            @keydown.enter="addPerson"
+            type="text"
+            placeholder="New Participant"
+          />
+
+          <button @click="addPerson">Add Person</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div>{{ formatNames() }}</div>
+
+  <div v-if="people.length > 1" class="card">
+    <h2>Enter Expenses:</h2>
+    <div>
+      <div>
+        <select v-model="selectedPerson" id="people" class="people-dropdown">
+          <option value="" disabled selected>Who paid?</option>
+          <option v-for="person in people" :key="person" :value="person">
+            {{ person }}
+          </option>
+        </select>
+        <input
+        v-model="personName"
+        @keydown.enter="addPerson"
+        type="text"
+        class="expense-input"
+        placeholder="How much?"
+      />
+      <button @click="addPerson">Add Expense</button>
+      </div>
+      
+    </div>
   </div>
 
   <p>
